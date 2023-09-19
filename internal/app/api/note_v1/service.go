6b8cf630f5bc169651e9898034f8b32d56220031
sam <you@example.com>
2023-09-20 02:56:26 +0300

    feat: fake database module

--- internal/app/api/note_v1/service.go
+++ internal/app/api/note_v1/service.go
@@ -1,11 +1,17 @@
 package note_v1
 
-import desc "github.com/MuhahaSam/golangPractice/pkg/note_v1"
+import (
+	db "github.com/MuhahaSam/golangPractice/internal/app/db"
+	desc "github.com/MuhahaSam/golangPractice/pkg/note_v1"
+)
 
 type Note struct {
 	desc.NoteServiceServer
 }
 
 func NewNote() *Note {
+
+	db.GetDbModuleInstance().Connect()
+
 	return &Note{}
 }
